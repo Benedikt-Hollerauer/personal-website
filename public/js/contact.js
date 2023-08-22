9eb05fa6a107f433d979edd9332494830800e761
Benedikt Hollerauer <88812164+Benedikt-Hollerauer@users.noreply.github.com>
2023-08-22 17:10:27 +0200

    moved contact form into component

--- public/js/contact.js
+++ public/js/contact.js
@@ -4,11 +4,3 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
 });
-
-$( '.js-input' ).keyup(function() {
-    if( $(this).val() ) {
-       $(this).addClass('not-empty');
-    } else {
-       $(this).removeClass('not-empty');
-    }
-});
