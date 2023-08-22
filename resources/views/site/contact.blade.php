0d9ab44340d833e00727bc081e5f4574ad2837d1
Benedikt Hollerauer <88812164+Benedikt-Hollerauer@users.noreply.github.com>
2023-08-22 10:48:04 +0200

    cleaned up css and added contact page

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -10,22 +10,13 @@
         <title>Benedikt Hollerauer</title>
     </head>
     <body>
-        <div class="home-container">
+        <div class="contact-container">
             <a href="{{ url(''); }}" class="top">
                 <x-navigation-arrow :rotation="NavigationArrowDirection::UP"/>
-            </a>
-            <a href="{{ url(''); }}" class="left">
-                <x-navigation-arrow :rotation="NavigationArrowDirection::LEFT"/>
             </a>
             <div class="main center-items">
                 content
             </div>
-            <a href="{{ url(''); }}" class="right">
-                <x-navigation-arrow :rotation="NavigationArrowDirection::RIGHT"/>
-            </a>
-            <a href="{{ url(''); }}/contact"class="bottom">
-                <x-navigation-arrow :rotation="NavigationArrowDirection::DOWN"/>
-            </a>
         </div>
     </body>
 </html>
