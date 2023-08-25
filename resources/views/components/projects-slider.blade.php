729c65e50cb40a2acf77757fe7eae9fc4a126c2c
Benedikt Hollerauer <88812164+Benedikt-Hollerauer@users.noreply.github.com>
2023-08-25 17:10:04 +0200

    added a projects slider

---
+++ resources/views/components/projects-slider.blade.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <input type="radio" name="slider" id="item-1" checked>
+    <input type="radio" name="slider" id="item-2">
+    <input type="radio" name="slider" id="item-3">
+    <div class="cards">
+        <label class="card center-items" for="item-1" id="song-1">
+            first
+        </label>
+        <label class="card center-items" for="item-2" id="song-2">
+            second
+        </label>
+        <label class="card center-items" for="item-3" id="song-3">
+            third
+        </label>
+    </div>
+</div>
