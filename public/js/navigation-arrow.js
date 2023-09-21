9bc78bb22cb986c54e387248ace69737b854f014
Benedikt Hollerauer <hollibene@gmail.com>
2023-09-21 21:27:17 +0200

    started to add transfroming navigation arrows

---
+++ public/js/navigation-arrow.js
@@ -0,0 +1,13 @@
+//function changeNavigationArrowImageSource(
+//    newImageSource,
+//    imageId,
+//    rotation
+//) {
+//    document.getElementById(
+//        imageId
+//    ).src = newImageSource;
+//    document.getElementById("link-" + rotation)
+//        .style
+//        .transform
+//        .rotation = 0
+//}
--- resources/views/components/navigation-arrow.blade.php
+++ resources/views/components/navigation-arrow.blade.php
@@ -1,7 +1,12 @@
-<a href="{{ $linkLocation }}" class="{{ $rotation->getRotationName() }}">  <!-- wire:navigate -->
-    <div class="navigation-item center-items">
-        <div id="navigation-arrow" style="transform: rotate({{ $rotation->getDegreeForRotation() }}deg);">
-            <img src="icon/navigation-arrow.svg" alt="Navigation Arrow Icon"/>
+<script src="js/navigation-arrow.js"></script>
+<a id="link-{{ $rotation->getRotationName() }}" href="{{ $linkLocation }}" class="{{ $rotation->getRotationName() }}">
+    <div class="navigation-item center-items" onmouseover="changeNavigationArrowImageSource(
+            'icon/home.svg',
+            'navigation-arrow-{{ $rotation->getRotationName() }}',
+            '{{ $rotation->getRotationName() }}')"
+        >
+        <div id="navigation-arrow-{{ $rotation->getRotationName() }}" style="transform: rotate({{ $rotation->getDegreeForRotation() }}deg);">
+            <img id="navigation-arrow-img" src="icon/navigation-arrow.svg" alt="Navigation Icon"/>
         </div>
     </div>
 </a>
