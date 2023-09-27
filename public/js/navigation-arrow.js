e1172b903756caeca072c91ea74347c107b2346d
Benedikt Hollerauer <88812164+Benedikt-Hollerauer@users.noreply.github.com>
2023-09-27 11:27:21 +0200

    added events to change navigation arrow on hover

--- public/js/navigation-arrow.js
+++ public/js/navigation-arrow.js
@@ -1,13 +1,24 @@
-//function changeNavigationArrowImageSource(
-//    newImageSource,
-//    imageId,
-//    rotation
-//) {
-//    document.getElementById(
-//        imageId
-//    ).src = newImageSource;
-//    document.getElementById("link-" + rotation)
-//        .style
-//        .transform
-//        .rotation = 0
-//}
+function changeNavigationArrowImageSource(
+    newImageSource,
+    imageId,
+    rotation
+) {
+    document.getElementById(
+        imageId
+    ).src = newImageSource;
+    document.getElementById("link-" + rotation)
+        .style
+        .transform
+        .rotation = 0
+}
+
+function restoreNavigationArrow(
+    imageId,
+    rotationDegree
+) {
+    let image = document.getElementById(imageId)
+    image.src = "icon/navigation-arrow.svg"
+    image.style
+        .transform
+        .rotation = rotationDegree
+}
--- resources/views/components/navigation-arrow.blade.php
+++ resources/views/components/navigation-arrow.blade.php
@@ -1,12 +1,18 @@
 <script src="js/navigation-arrow.js"></script>
 <a id="link-{{ $rotation->getRotationName() }}" href="{{ $linkLocation }}" class="{{ $rotation->getRotationName() }}">
-    <div class="navigation-item center-items" onmouseover="changeNavigationArrowImageSource(
+    <div class="navigation-item center-items"
+        onmouseover="changeNavigationArrowImageSource(
             'icon/home.svg',
             'navigation-arrow-{{ $rotation->getRotationName() }}',
-            '{{ $rotation->getRotationName() }}')"
+            '{{ $rotation->getRotationName() }}'
+        )"
+        onmouseout="restoreNavigationArrow(
+            'navigation-arrow-{{ $rotation->getRotationName() }}',
+            '{{ $rotation->getDegreeForRotation() }}'
+        )"
         >
-        <div id="navigation-arrow-{{ $rotation->getRotationName() }}" style="transform: rotate({{ $rotation->getDegreeForRotation() }}deg);">
-            <img id="navigation-arrow-img" src="icon/navigation-arrow.svg" alt="Navigation Icon"/>
+        <div style="transform: rotate({{ $rotation->getDegreeForRotation() }}deg);">
+            <img id="navigation-arrow-{{ $rotation->getRotationName() }}" class="navigation-arrow-img" src="icon/navigation-arrow.svg" alt="Navigation Icon"/>
         </div>
     </div>
 </a>
