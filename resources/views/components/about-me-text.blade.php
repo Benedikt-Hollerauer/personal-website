4de0f901274cb8b043e062937bf02c9be1ea4b8b
Benedikt Hollerauer <hollibene@gmail.com>
2023-10-02 10:24:15 +0200

    added text-link styling

--- resources/views/components/about-me-text.blade.php
+++ resources/views/components/about-me-text.blade.php
@@ -3,6 +3,6 @@
         Hi, my name is {{ config('constants.MY_NICK_NAME') }}.
         <br>
         I`m {{ config('constants.MY_CURRENT_AGE') }} years old and I`m living in Bavaria / Germany.
-        Currently I`m a software developer working at <a href="{{ config('constants.MY_CURRENT_WORKPLACE_LINK') }}">{{ config('constants.MY_CURRENT_WORKPLACE') }}</a>. 
+        Currently I`m a software developer working at <a href="{{ config('constants.MY_CURRENT_WORKPLACE_LINK') }}" target="_blank" class="text-link">{{ config('constants.MY_CURRENT_WORKPLACE') }}</a>. 
     </div>
 </div>
