315df1377a46f71c027e8b20b5334ddbdd4ea010
Benedikt Hollerauer <hollibene@gmail.com>
2023-10-05 16:00:24 +0200

    added tdd logo to skill cards and added alt texts

--- resources/views/components/skill-cards.blade.php
+++ resources/views/components/skill-cards.blade.php
@@ -1,7 +1,7 @@
 <div class="cards-list">
   <a href="https://www.scala-lang.org/" target="_blank" class="card 1">
     <div class="card_image">
-      <img src="icons/scala-logo.svg" />
+      <img src="icons/scala-logo.svg" alt="Scala Logo"/>
     </div>
     <!--<div class="card_title title-black">
       <p>Scala</p>
@@ -10,7 +10,7 @@
 
   <a href="https://www.scala-js.org/" target="_blank" class="card 1">
     <div class="card_image">
-      <img src="icons/scala-js-logo.svg"/>
+      <img src="icons/scala-js-logo.svg" alt="Scala.JS Logo"/>
     </div>
     <!--<div class="card_title title-white">
       <p>Scala.JS</p>
@@ -19,7 +19,7 @@
 
   <a href="https://zio.dev/" target="_blank" class="card 1">
     <div class="card_image">
-      <img src="icons/zio-logo.png"/>
+      <img src="icons/zio-logo.png" alt="ZIO Logo"/>
     </div>
     <!--<div class="card_title title-white">
       <p>ZIO</p>
@@ -28,7 +28,7 @@
   
   <a href="https://www.php.net/" target="_blank" class="card 1">
     <div class="card_image">
-      <img src="icons/php-logo.svg"/>
+      <img src="icons/php-logo.svg" alt="PHP Logo"/>
     </div>
     <!--<div class="card_title title-white">
       <p>PHP</p>
@@ -37,7 +37,7 @@
 
   <a href="https://laravel.com/" target="_blank" class="card 1">
     <div class="card_image">
-      <img src="icons/laravel-logo.svg"/>
+      <img src="icons/laravel-logo.svg" alt="Laravel Logo"/>
     </div>
     <!--<div class="card_title title-white">
       <p>PHP</p>
@@ -46,7 +46,7 @@
 
   <a href="https://www.mysql.com/" target="_blank" class="card 2">
     <div class="card_image">
-      <img src="icons/mysql-logo.svg" />
+      <img src="icons/mysql-logo.svg" alt="MySQL Logo"/>
     </div>
     <!--<div class="card_title title-white">
       <p>SQL</p>
@@ -55,15 +55,25 @@
 
   <a href="/resources" target="_blank" class="card 3">
     <div class="card_image">
-      <img src="icons/linux-logo.svg" />
+      <img src="icons/linux-logo.svg" alt="Linux Logo"/>
     </div>
     <!--<div class="card_title title-white">
       <p>Linux</p>
     </div>-->
     </a>
+
+    <a href="https://en.wikipedia.org/wiki/Test-driven_development" target="_blank" class="card 3">
+    <div class="card_image">
+      <img src="icons/tdd-logo.png" alt="TDD Logo"/>
+    </div>
+    <!--<div class="card_title title-white">
+      <p>Linux</p>
+    </div>-->
+    </a>
+
   <a href="https://www.docker.com/" target="_blank" class="card 4">
     <div class="card_image">
-      <img src="icons/docker-logo.svg" />
+      <img src="icons/docker-logo.svg" alt="Docker Logo"/>
     </div>
     <!--<div class="card_title title-white">
       <p>Docker</p>
