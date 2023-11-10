b2305ec260583115acfd3ac48ed79dcaf258dd7c
Benedikt Hollerauer <ho@boerse-group.de>
2023-11-10 16:45:46 +0100

    better styling

--- resources/views/sites/contact.blade.php
+++ resources/views/sites/contact.blade.php
@@ -3,17 +3,16 @@
 ?>
 <x-site-template>
     <x-slot:siteTitle>Contact - {{ config('constants.MY_FULL_NAME') }}</x-slot>
-    <div class="contact-container">
-        <x-navigation-arrow
-            :rotation="NavigationArrowDirection::UP"
-            linkLocation="/"
-            iconOnHover="'icons/home.svg'"
-        />
-        <div class="contact-form-site-wrapper main center-items">
-            <x-background-card>
-                <x-contact-form/>
-            </x-background-card>
+        <div class="contact-container">
+            <x-navigation-arrow :rotation="NavigationArrowDirection::UP" linkLocation="/"
+                iconOnHover="'icons/home.svg'" />
+            <div class="contact-form-site-wrapper main center-items">
+                <div id="wrapper-contact-form-background-card">
+                    <x-background-card>
+                        <x-contact-form />
+                    </x-background-card>
+                </div>
+            </div>
+            <x-site-heading siteHeading="Contact" />
         </div>
-        <x-site-heading siteHeading="Contact"/>
-    </div>
 </x-site-template>
--- resources/views/sites/projects.blade.php
+++ resources/views/sites/projects.blade.php
@@ -3,15 +3,12 @@
 ?>
 <x-site-template>
     <x-slot:siteTitle>Projects - {{ config('constants.MY_FULL_NAME') }}</x-slot>
-    <div class="projects-container">
-        <x-site-heading siteHeading="Projects"/>
-        <x-navigation-arrow
-            :rotation="NavigationArrowDirection::LEFT"
-            linkLocation="/"
-            iconOnHover="'icons/home.svg'"
-        />
-        <div class="main center-items">
-            <x-projects-slider/>
+        <div class="projects-container">
+            <x-site-heading siteHeading="Projects" />
+            <x-navigation-arrow :rotation="NavigationArrowDirection::LEFT" linkLocation="/"
+                iconOnHover="'icons/home.svg'" />
+            <div class="main center-items">
+                <x-projects-slider />
+            </div>
         </div>
-    </div>
 </x-site-template>
