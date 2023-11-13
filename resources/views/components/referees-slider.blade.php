c0216fcc9e177b738555f3ddb43087bfff09db32
Benedikt Hollerauer <ho@boerse-group.de>
2023-11-13 16:24:23 +0100

    changed testimonials styling

--- resources/views/components/referees-slider.blade.php
+++ resources/views/components/referees-slider.blade.php
@@ -3,30 +3,20 @@
     <div class="testimonial">
         <!-- PROFILE INFO -->
         <div class="profile-info">
-            <img src="https://gorgeous-dolphin-b58832.netlify.app/images/image-daniel.jpg" alt="Daniel pfp" />
+            <img src="icons/quotes-icon.svg" alt="Quotes" />
             <div class="profile-text">
-                <h2 class="name">Daniel Clifford</h2>
-                <p>Verified Graduate</p>
+                <h2 class="name">Thomas Höfer</h2>
+                <p>Senior Software Engineer</p>
             </div>
         </div>
 
         <!-- TESTIMONIAL -->
         <div class="testimonial-text">
             <h3>
-                I received a job offer mid-course, and the subjects I learned were
-                current, if not more so, in the company I joined. I honestly feel I
-                got every penny’s worth.
+                As Bene's former superior at Börse.de Group AG, I confidently recommend him as a software engineer. He
+                excels in creating robust code, promoting teamwork, and quickly adapting to new technologies, benefiting
+                our projects significantly.
             </h3>
-
-            <p>
-                “ I was an EMT for many years before I joined the bootcamp. I’ve
-                been looking to make a transition and have heard some people who had
-                an amazing experience here. I signed up for the free intro course
-                and found it incredibly fun! I enrolled shortly thereafter. The next
-                12 weeks was the best - and most grueling - time of my life. Since
-                completing the course, I’ve successfully switched careers, working
-                as a Software Engineer at a VR startup. ”
-            </p>
         </div>
     </div>
 
@@ -34,22 +24,20 @@
     <div class="testimonial">
         <!-- PROFILE INFO -->
         <div class="profile-info">
-            <img src="https://6476867bca65513195636a27--gorgeous-dolphin-b58832.netlify.app/images/image-jonathan.jpg"
-                alt="jonathan pfp" />
+            <img src="icons/quotes-icon.svg"
+                alt="Quotes" />
             <div class="profile-text">
-                <h2 class="name">Jonathan Walters</h2>
-                <p>Verified Graduate</p>
+                <h2 class="name">Markus Ziegler</h2>
+                <p>Senior Software Engineer</p>
             </div>
         </div>
 
         <!-- TESTIMONIAL -->
         <div class="testimonial-text">
-            <h3>The team was very supportive and kept me motivated</h3>
-            <p>
-                “ I started as a total newbie with virtually no coding skills. I now
-                work as a mobile engineer for a big company. This was one of the
-                best investments I’ve made in myself. ”
-            </p>
+            <h3>Bene is fast and efficient in managing tasks. His quick wits allow him to adapt to unforseen situations
+                and deliver quality code. As his mentor in vocational training, I can recommend him as a valuable
+                talent, software developer and team member.
+            </h3>
         </div>
     </div>
 
@@ -57,21 +45,19 @@
     <div class="testimonial">
         <!-- PROFILE INFO -->
         <div class="profile-info">
-            <img src="https://6476867bca65513195636a27--gorgeous-dolphin-b58832.netlify.app/images/image-jeanette.jpg"
-                alt="jeanette pfp" />
+            <img src="icons/quotes-icon.svg"
+                alt="  font-size: larger;" />
             <div class="profile-text">
-                <h2 class="name">Jeanette Harmon</h2>
-                <p>Verified Graduate</p>
+                <h2 class="name">Jannik Meier</h2>
+                <p>Software Developer</p>
             </div>
         </div>
 
         <!-- TESTIMONIAL -->
         <div class="testimonial-text">
-            <h3>An overall wonderful and rewarding experience</h3>
-            <p>
-                “ Thank you for the wonderful experience! I now have a job I really
-                enjoy, and make a good living while doing something I love. ”
-            </p>
+            <h3>Currently (2023), I'm in the second year of my apprenticeship, with Bene one year ahead of me, serving
+                as my superior. He always helps me if I have questions regarding our codebase and has taught me the
+                details in an understandable way. Overall, he is a good teacher and developer.</h3>
         </div>
     </div>
 
@@ -79,63 +65,21 @@
     <div class="testimonial">
         <!-- PROFILE INFO -->
         <div class="profile-info">
-            <img src="https://6476867bca65513195636a27--gorgeous-dolphin-b58832.netlify.app/images/image-patrick.jpg"
-                alt="patrick pfp" />
+            <img src="icons/quotes-icon.svg"
+                alt="Quotes" />
             <div class="profile-text">
-                <h2 class="name">Patrick Abrams</h2>
-                <p>Verified Graduate</p>
+                <h2 class="name">Kevin Heick</h2>
+                <p>Senior Software Engineer</p>
             </div>
         </div>
 
         <!-- TESTIMONIAL -->
         <div class="testimonial-text">
             <h3>
-                Awesome teaching support from TAs who did the bootcamp themselves.
-                Getting guidance from them and learning from their experiences was
-                easy.
+                I started my apprenticeship with Bene at the same time. Working with Bene is great because he is not
+                only a skilled developer but also an excellent member of our team. Bene creates a positive work
+                atmosphere where we can share jokes while maintaining efficient collaboration on our projects.
             </h3>
-
-            <p>
-                “ The staff seem genuinely concerned about my progress which I find
-                really refreshing. The program gave me the confidence necessary to
-                be able to go out in the world and present myself as a capable
-                junior developer. The standard is above the rest. You will get the
-                personal attention you need from an incredible community of smart
-                and amazing people. ”
-            </p>
-        </div>
-    </div>
-
-    <!-- TESTIMONIAL ITEM-5-->
-    <div class="testimonial">
-        <!-- PROFILE INFO -->
-        <div class="profile-info">
-            <img src="https://6476867bca65513195636a27--gorgeous-dolphin-b58832.netlify.app/images/image-kira.jpg"
-                alt="danial pfp" />
-            <div class="profile-text">
-                <h2 class="name">Kira Whittle</h2>
-                <p>Verified Graduate</p>
-            </div>
-        </div>
-
-        <!-- TESTIMONIAL -->
-        <div class="testimonial-text">
-            <h3>Such a life-changing experience. Highly recommended!</h3>
-
-            <p>
-                “ Before joining the bootcamp, I’ve never written a line of code. I
-                needed some structure from professionals who can help me learn
-                programming step by step. I was encouraged to enroll by a former
-                student of theirs who can only say wonderful things about the
-                program. The entire curriculum and staff did not disappoint. They
-                were very hands-on and I never had to wait long for assistance. The
-                agile team project, in particular, was outstanding. It took my
-                learning to the next level in a way that no tutorial could ever
-                have. In fact, I’ve often referred to it during interviews as an
-                example of my developent experience. It certainly helped me land a
-                job as a full-stack developer after receiving multiple offers. 100%
-                recommend! ”
-            </p>
         </div>
     </div>
 </div>
