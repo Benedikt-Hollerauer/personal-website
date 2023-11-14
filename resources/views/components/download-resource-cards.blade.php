9db184db1516925a19e941a766a1629c935baa84
Benedikt Hollerauer <ho@boerse-group.de>
2023-11-14 17:27:26 +0100

    small website wide styling changes

--- resources/views/components/download-resource-cards.blade.php
+++ resources/views/components/download-resource-cards.blade.php
@@ -2,7 +2,7 @@
   <div class="download-resource-cards-container">
     <main class="grid">
       <article>
-        <div class="text">
+        <div class="text-download-cards">
           <h3>✅ Scala & Functional Programming Advanced Certificate</h3>
           <br>
           <p>This incredible course from <a class="text-link" href="https://rockthejvm.com/" target="_blank">Rock the JVM</a>, founded by Daniel Ciocîrlan, taught me advanced features of the language <a class="text-link" href="https://www.scala-lang.org/" target="_blank">Scala</a>, and, foremost, advanced functional programming concepts.</p>
@@ -11,7 +11,7 @@
       </article>
 
       <article>
-        <div class="text">
+        <div class="text-download-cards">
           <h3>✅ LPI - Linux Certificate</h3>
           <br>
           <p>To obtain <a class="text-link" href="https://www.lpi.org/our-certifications/linux-essentials-overview/" target="_blank">this certificate</a>, I developed a comprehensive understanding of Linux. This certificate is issued by the reputable and widely recognized <a class="text-link" href="https://www.lpi.org/" target="_blank">Linux Professional Institute (LPI).</a></p>
@@ -20,7 +20,7 @@
       </article>
       <article>
 
-        <div class="text">
+        <div class="text-download-cards">
           <h3>✅ Scala & Functional Programming Essentials Certificate</h3>
           <br>
           <p>This excellent course, offered by <a class="text-link" target="_blank" href="https://rockthejvm.com/">Rock the JVM</a> and led by its founder, Daniel Ciocîrlan, provided me with a solid foundation in the <a class="text-link" target="_blank" href="https://www.scala-lang.org/">Scala</a> programming language. Moreover, it was my gateway to the fascinating realm of functional programming.</p>
@@ -28,7 +28,7 @@
         </div>
       </article>
       <article>
-        <div class="text">
+        <div class="text-download-cards">
           <h3>✅ My CV</h3>
           <br>
           <p>Download my CV here to learn more about my skills and my experience.</p>
@@ -37,7 +37,7 @@
       </article>
 
       <article>
-        <div class="text">
+        <div class="text-download-cards">
           <h3>✅ Europass</h3>
           <br>
           <p>Here you can receive my <a class="text-link" target="_blank" href="https://europa.eu/europass/en">Europass</a>, which is the official platform of the European Union for creating a standardized CV.</p>
