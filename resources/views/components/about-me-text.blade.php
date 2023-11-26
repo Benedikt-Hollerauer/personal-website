c7d5ddc1660174d71e39f0fc52d84a21fb3a6aba
Benedikt Hollerauer <hollibene@gmail.com>
2023-11-26 20:03:00 +0100

    refactored about me text

--- resources/views/components/about-me-text.blade.php
+++ resources/views/components/about-me-text.blade.php
@@ -3,9 +3,11 @@
         <p>👋 Hey there, I'm <strong>{{ config('constants.MY_NICK_NAME') }}</strong>,</p>
         <br>
         <p>a software engineer based in Bavaria, Germany. 🌍</p>
-        <p>I'm passionate about transforming ideas into robust, well-tested software products. 🔧💡</p>
-        <p>Specializing in functional programming, I'm deeply interested in the architecture and design of software products. 🛠️</p>
-        <p>I invite you to explore my website and connect with me on social media. 🌐📱</p>
+        <p>I am passionate about turning ideas into robust and well-tested software products. 🔧💡</p>
+        <p>My expertise lies in functional programming, and I have a deep interest in the architecture and design of software products. 🛠️</p>
+		<br>
+        <p>Currently, I am proficient in Scala, Java, and PHP, and I am dedicated to expanding my skills in Rust. Continuous learning is important to me, and I enjoy taking on new challenges. With my strong teamwork and agile development skills, I strive for success in every project. 🚀🤝</p>
+		<br>
         <p>Cheers! 🥂</p>
     </div>
 </div>
