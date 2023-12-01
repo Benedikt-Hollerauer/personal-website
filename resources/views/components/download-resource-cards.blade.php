1d63582b23ee62ca6b91db69d8bd56d95dd7967c
Benedikt Hollerauer <ho@boerse-group.de>
2023-12-01 12:06:10 +0100

    removed europass and german cv download cards

--- resources/views/components/download-resource-cards.blade.php
+++ resources/views/components/download-resource-cards.blade.php
@@ -12,14 +12,6 @@
 
       <article>
         <div class="text-download-cards">
-          <h3>🇩🇪 My German CV</h3>
-          <br>
-          <p>If you are from Germany, I have a German CV available for download here.</p>
-          <a href="/downloadGermanCV" class="btn">Download</a>
-        </div>
-      </article>
-      <article>
-        <div class="text-download-cards">
           <h3>🎓 Scala & Functional Programming Advanced Certificate</h3>
           <br>
           <p>This incredible course from <a class="text-link" href="https://rockthejvm.com/" target="_blank">Rock the JVM</a>, founded by Daniel Ciocîrlan, taught me advanced features of the language <a class="text-link" href="https://www.scala-lang.org/" target="_blank">Scala</a>, and, foremost, advanced functional programming concepts.</p>
@@ -42,15 +34,6 @@
           <br>
           <p>This excellent course, offered by <a class="text-link" target="_blank" href="https://rockthejvm.com/">Rock the JVM</a> and led by its founder, Daniel Ciocîrlan, provided me with a solid foundation in the <a class="text-link" target="_blank" href="https://www.scala-lang.org/">Scala</a> programming language. Moreover, it was my gateway to the fascinating realm of functional programming.</p>
           <a href="/downloadScalaEssentialsCertificate" class="btn">Download</a>
-        </div>
-      </article>
-      
-      <article>
-        <div class="text-download-cards">
-          <h3>🇪🇺 Europass</h3>
-          <br>
-          <p>Here you can receive my <a class="text-link" target="_blank" href="https://europa.eu/europass/en">Europass</a>, which is the official platform of the European Union for creating a standardized CV.</p>
-          <a href="/downloadEuropass" class="btn">Download</a>
         </div>
       </article>
     </main>
