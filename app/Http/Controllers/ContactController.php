5da8a46a8f2f2d2e8b0679e1e013b20181601f15
Benedikt Hollerauer <benedikt.hollerauer@skiff.com>
2024-04-01 21:28:47 +0200

    added better error message to email send failure

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -27,7 +27,7 @@ class ContactController extends Controller
     public function emailSentFailure(): View
     {
         return view('sites.email-sent', [
-            'message' => "⛔ Please try again or send me a direct email to \n " . config('constants.MY_EMAIL_ADDRESS') . " ⛔"
+            'message' => "⛔ Please try again or send me a direct email ⛔"
         ]);
     }
 
