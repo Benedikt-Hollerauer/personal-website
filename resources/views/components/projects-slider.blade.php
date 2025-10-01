4c36797a97570d84e1fe07329563756eda505161
Benedikt Hollerauer <benedikt.hollerauer@braintower.de>
2025-10-01 14:14:05 +0200

    added automaed confluence cloud export poject and removed other projects

--- resources/views/components/projects-slider.blade.php
+++ resources/views/components/projects-slider.blade.php
@@ -18,6 +18,17 @@
 
     <div class="blog-slider__item swiper-slide">
       <div class="blog-slider__img">
+        <img src="images/confluence-logo.svg" alt="Leetcode icon">
+      </div>
+      <div class="blog-slider__content">
+        <div class="blog-slider__title">AUTOMATED CONFLUENCE CLOUD EXPORT</div>
+        <div class="blog-slider__text">🔄 The Automated Confluence Cloud Export automates documentation backup using Python and Playwright. Built with functional programming patterns, RPM packaging, Systemd timers, and GitLab CI/CD pipelines for reliable scheduled exports. 📁</div>
+        <!--<a href="https://github.com/Benedikt-Hollerauer/leetcode" target="_blank" class="blog-slider__button">Check out the code here</a>-->
+      </div>
+    </div>
+
+    <div class="blog-slider__item swiper-slide">
+      <div class="blog-slider__img">
 
         <img src="images/sorting-symbolic-logo.svg" alt="Sorting algorithm visualizer symbolic logo">
       </div>
@@ -25,39 +36,6 @@
         <div class="blog-slider__title">SORTING ALGORITHM VISUALIZER</div>
         <div class="blog-slider__text">📶 The Sorting Algorithm Visualizer project demonstrates various sorting algorithms using higher-order functions in Scala.js. It follows pure functional programming principles, adheres to Test-Driven Development (TDD) and Clean Architecture, providing a visual representation of algorithm functionality. 📊</div>
         <a href="/sorting-algorithm-visualizer" target="_blank" class="blog-slider__button">Check it out here</a>
-      </div>
-    </div>
-
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        <img src="images/leetcode-logo.svg" alt="Leetcode icon">
-      </div>
-      <div class="blog-slider__content">
-        <div class="blog-slider__title">LEETCODE</div>
-        <div class="blog-slider__text">🏋️‍♂️ This is my LeetCode practice repository! This repository is dedicated to my journey of training algorithms and data structures. Here, you will find a collection of my solutions to LeetCode problems. 🧠</div>
-        <a href="https://github.com/Benedikt-Hollerauer/leetcode" target="_blank" class="blog-slider__button">Check out the code here</a>
-      </div>
-    </div>
-
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        <img src="images/sales-monitoring-symbolic-logo.svg" alt="Sales monitoring symbolic image">
-      </div>
-      <div class="blog-slider__content">
-        <div class="blog-slider__title">SALES MONITORING</div>
-        <div class="blog-slider__text">💼 The Sales Monitoring project is coded in Scala and compiled through Scala.js, following the Clean Architecture principles by Uncle Bob (Robert C. Martin). It offers features such as profit calculation, viewing the latest sales, creating new sales records, and searching for sales data. 📈</div>
-        <a href="https://github.com/Benedikt-Hollerauer/sales-monitoring" target="_blank" class="blog-slider__button">Check out the code here</a>
-      </div>
-    </div>
-
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        <img src="images/website-symbolic-logo.svg" alt="Personal website symbolic logo">
-      </div>
-      <div class="blog-slider__content">
-        <div class="blog-slider__title">PERSONAL WEBSITE</div>
-        <div class="blog-slider__text">🌐 This is my personal website! This site, built with PHP and the Laravel framework, reflects my growth in web development. While creating this project, I explored different frameworks and libraries, improving my ability to make strong and interactive websites. 🚀</div>
-        <a href="https://github.com/Benedikt-Hollerauer/personal-website" target="_blank" class="blog-slider__button">Check out the code here</a>
       </div>
     </div>
 
