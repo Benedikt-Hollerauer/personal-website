8108d5ca48cab3329d3c54c23539b984561ecf98
Benedikt Hollerauer <benedikt.hollerauer@braintower.de>
2025-10-02 16:19:51 +0200

    updated skill cards

--- resources/views/components/skill-cards.blade.php
+++ resources/views/components/skill-cards.blade.php
@@ -1,68 +1,68 @@
 <div class="cards-list">
-  <a href="https://www.scala-lang.org/" target="_blank" class="card 1">
+  <a href="https://dev.to/adityapratapbh1/principles-of-robust-software-architecture-design-building-strong-foundations-cmh" target="_blank" class="card 1">
     <div class="card_image">
-      <img src="images/scala-logo.svg" alt="Scala Logo" />
+      <img src="images/software-engineering-logo.svg" alt="Robust Software Engineering Logo" />
     </div>
-    <div class="tooltip">Scala</div>
+    <div class="tooltip">Robust Software Engineering</div>
   </a>
   
-  <a href="https://www.typescriptlang.org/" target="_blank" class="card 1">
-    <div class="card_image">
-      <img src="images/typescript-logo.svg" alt="Typescript Logo" />
-    </div>
-    <div class="tooltip">TypeScript</div>
-  </a>
-  
-  <a href="https://www.python.org/" target="_blank" class="card 1">
+  <a href="https://www.python.org" target="_blank" class="card 1">
     <div class="card_image">
       <img src="images/python-logo.svg" alt="Python Logo" />
     </div>
     <div class="tooltip">Python</div>
   </a>
   
-  <a href="https://reactjs.org/" target="_blank" class="card 2">
+  <a href="https://en.wikipedia.org/wiki/Functional_programming" target="_blank" class="card 1">
     <div class="card_image">
-      <img src="images/react-logo.svg" alt="React Logo" />
+      <img src="images/functional-programming-icon.svg" alt="Functional Programming Icon" />
     </div>
-    <div class="tooltip">React</div>
+    <div class="tooltip">Functional Programming</div>
   </a>
   
-  <a href="https://www.docker.com/" target="_blank" class="card 2">
+  <a href="https://www.coursera.org/articles/ai-integration" target="_blank" class="card 2">
+    <div class="card_image">
+      <img src="images/ai-logo.svg" alt="AI Integration Logo" />
+    </div>
+    <div class="tooltip">AI Integration</div>
+  </a>
+  
+  <a href="https://www.docker.com" target="_blank" class="card 2">
     <div class="card_image">
       <img src="images/docker-logo.svg" alt="Docker Logo" />
     </div>
     <div class="tooltip">Docker</div>
   </a>
   
-  <a href="https://kubernetes.io/" target="_blank" class="card 3">
+  <a href="https://about.gitlab.com/topics/ci-cd" target="_blank" class="card 3">
     <div class="card_image">
-      <img src="images/kubernetes-logo.svg" alt="Kubernetes Logo" />
+      <img src="images/ci-cd-icon.svg" alt="CI/CD Logo" />
     </div>
-    <div class="tooltip">Kubernetes</div>
+    <div class="tooltip">CI/CD</div>
   </a>
   
-  <a href="https://aws.amazon.com/" target="_blank" class="card 3">
+  <a href="https://rust-lang.org" target="_blank" class="card 3">
     <div class="card_image">
-      <img src="images/aws-logo.svg" alt="AWS Logo" />
+      <img src="images/rust-logo.svg" alt="Rust Logo" />
     </div>
-    <div class="tooltip">AWS</div>
+    <div class="tooltip">Rust</div>
   </a>
   
-  <a href="https://git-scm.com/" target="_blank" class="card 4">
+  <a href="https://git-scm.com" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/git-logo.svg" alt="Git Logo" />
     </div>
     <div class="tooltip">Git</div>
   </a>
   
-  <a href="https://www.mysql.com/" target="_blank" class="card 4">
+  <a href="https://www.mysql.com" target="_blank" class="card 4">
     <div class="card_image">
-      <img src="images/mysql-logo.svg" alt="MySQL Logo" />
+      <img src="images/sql-logo.svg" alt="SQL Logo" />
     </div>
-    <div class="tooltip">MySQL</div>
+    <div class="tooltip">SQL</div>
   </a>
   
-  <a href="https://www.linkedin.com/in/benedikt-hollerauer/details/skills/" target="_blank" class="card more-skills">
+  <a href="https://www.linkedin.com/in/benedikt-hollerauer/details/skills" target="_blank" class="card more-skills">
     <div class="card_image">
       <div class="more-skills-content">
         <span class="plus-icon">+</span>
