01cb5131ac20a869d6d5eeef60b2e96407065b35
Benedikt Hollerauer <benedikt.hollerauer@braintower.de>
2025-10-02 15:29:21 +0200

    added tool tips for skill cards

--- resources/views/components/skill-cards.blade.php
+++ resources/views/components/skill-cards.blade.php
@@ -3,151 +3,132 @@
     <div class="card_image">
       <img src="images/scala-logo.svg" alt="Scala Logo" />
     </div>
-    <!--<div class="card_title title-black">
-      <p>Scala</p>
-    </div>-->
+    <div class="tooltip">Scala</div>
   </a>
-
+  
   <a href="https://www.scala-js.org/" target="_blank" class="card 1">
     <div class="card_image">
       <img src="images/scala-js-logo.svg" alt="Scala.JS Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>Scala.JS</p>
-    </div>-->
+    <div class="tooltip">Scala.JS</div>
   </a>
-
+  
   <a href="https://zio.dev/" target="_blank" class="card 1">
     <div class="card_image">
       <img src="images/zio-logo.png" alt="ZIO Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>ZIO</p>
-    </div>-->
+    <div class="tooltip">ZIO</div>
   </a>
-
+  
   <a href="https://www.php.net/" target="_blank" class="card 1">
     <div class="card_image">
       <img src="images/php-logo.svg" alt="PHP Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>PHP</p>
-    </div>-->
+    <div class="tooltip">PHP</div>
   </a>
-
+  
   <a href="https://laravel.com/" target="_blank" class="card 1">
     <div class="card_image">
       <img src="images/laravel-logo.svg" alt="Laravel Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>PHP</p>
-    </div>-->
+    <div class="tooltip">Laravel</div>
   </a>
-
+  
   <a href="https://www.typescriptlang.org/" target="_blank" class="card 1">
     <div class="card_image">
       <img src="images/typescript-logo.svg" alt="Typescript Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>PHP</p>
-    </div>-->
+    <div class="tooltip">TypeScript</div>
   </a>
-
+  
   <a href="https://www.javascript.com/" target="_blank" class="card 1">
     <div class="card_image">
       <img src="images/javascript-logo.svg" alt="Javascript Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>PHP</p>
-    </div>-->
+    <div class="tooltip">JavaScript</div>
   </a>
-
+  
   <a href="https://www.mysql.com/" target="_blank" class="card 2">
     <div class="card_image">
       <img src="images/mysql-logo.svg" alt="MySQL Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>SQL</p>
-    </div>-->
+    <div class="tooltip">MySQL</div>
   </a>
-
+  
   <a href="/resources" class="card 3">
     <div class="card_image">
       <img src="images/linux-logo.svg" alt="Linux Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>Linux</p>
-    </div>-->
+    <div class="tooltip">Linux</div>
   </a>
-
+  
   <a href="https://en.wikipedia.org/wiki/Test-driven_development" target="_blank" class="card 3">
     <div class="card_image">
       <img src="images/tdd-logo.png" alt="TDD Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>Linux</p>
-    </div>-->
+    <div class="tooltip">TDD</div>
   </a>
-
+  
   <a href="https://www.docker.com/" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/docker-logo.svg" alt="Docker Logo" />
     </div>
-    <!--<div class="card_title title-white">
-      <p>Docker</p>
-    </div>-->
+    <div class="tooltip">Docker</div>
   </a>
+  
   <a href="https://www.rust-lang.org/" target="_blank" class="card 4">
     <div class="card_image">
-      <img src="images/rust-logo.svg" alt="Docker Logo" />
+      <img src="images/rust-logo.svg" alt="Rust Logo" />
     </div>
+    <div class="tooltip">Rust</div>
   </a>
-
+  
   <a href="https://git-scm.com/" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/git-logo.svg" alt="Git Logo" />
     </div>
+    <div class="tooltip">Git</div>
   </a>
-
+  
   <a href="https://www.python.org/" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/python-logo.svg" alt="Python Logo" />
     </div>
+    <div class="tooltip">Python</div>
   </a>
-
+  
   <a href="https://aws.amazon.com/" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/aws-logo.svg" alt="AWS Logo" />
     </div>
+    <div class="tooltip">AWS</div>
   </a>
-
+  
   <a href="https://kubernetes.io/" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/kubernetes-logo.svg" alt="Kubernetes Logo" />
     </div>
+    <div class="tooltip">Kubernetes</div>
   </a>
-
+  
   <a href="https://reactjs.org/" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/react-logo.svg" alt="React Logo" />
     </div>
+    <div class="tooltip">React</div>
   </a>
-
+  
   <a href="https://www.mongodb.com/" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/mongodb-logo.svg" alt="MongoDB Logo" />
     </div>
+    <div class="tooltip">MongoDB</div>
   </a>
-
+  
   <a href="https://redis.io/" target="_blank" class="card 4">
     <div class="card_image">
       <img src="images/redis-logo.svg" alt="Redis Logo" />
     </div>
-  </a>
-
-  <a href="https://redis.io/" target="_blank" class="card 4">
-    <div class="card_image">
-      <img src="images/redis-logo.svg" alt="Redis Logo" />
-    </div>
+    <div class="tooltip">Redis</div>
   </a>
 </div>
